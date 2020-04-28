626cd72abea64671ae4c4d26cc9918d370d5f813
Yael Balbastre <y.balbastre@ucl.ac.uk>
2020-04-28 10:55:35 +0100

    Feature(energy): Implement SPM's differential energies as convolution kernels.

--- nitorch/kernels.py
+++ nitorch/kernels.py
@@ -1,12 +1,34 @@
-"""Convolution kernels."""
+# -*- coding: utf-8 -*-
+"""Convolution kernels.
+
+@author: yael.balbastre@gmail.com
+"""
 
 # TODO:
 # . Implement Sinc kernel
-# . Implement differential energies
 
 import torch
+from nitorch import utils
 
-__all__ = ['smooth']
+__all__ = ['smooth', 'energy', 'energy1d', 'energy2d', 'energy3d',
+           'make_separable']
+
+
+def make_separable(ker, channels):
+    """Transform a single-channel kernel into a multi-channel separable kernel.
+
+    Args:
+        ker (torch.tensor): Single-channel kernel.
+        channels (int): Number of input/output channels.
+
+    Returns:
+        ker (torch.tensor): Multi-channel group kernel.
+
+    """
+    ndim = torch.as_tensor(ker.shape).numel()
+    repetitions = (channels,) + (1,)*(ndim-1)
+    ker = ker.repeat(repetitions)
+    return ker
 
 
 def integrate_poly(l, h, *args):
@@ -18,7 +40,7 @@ def integrate_poly(l, h, *args):
     All inputs should be `torch.Tensor`
     """
     # NOTE: operations are not performed inplace (+=, *=) so that autograd
-    # can backpropagated.
+    # can backpropagate.
     zero = torch.zeros(tuple(), dtype=torch.bool)
     k = torch.zeros(l.shape, dtype=l.dtype, device=l.device)
     hh = h
@@ -62,7 +84,7 @@ def gauss1d0(w, x):
     w1 = 1./(sqrttwo*s)
     ker = 0.5*((w1*(x+0.5)).erf() - (w1*(x-0.5)).erf())
     ker = ker.clamp(min=0)
-    return(ker, x)
+    return ker, x
 
 
 def gauss1d1(w, x):
@@ -84,7 +106,7 @@ def gauss1d1(w, x):
               + (w2*(x-1).pow(2)).exp()
               - 2*(w2*x.pow(2)).exp())
     ker = ker.clamp(min=0)
-    return(ker, x)
+    return ker, x
 
 
 def rect1d0(w, x):
@@ -94,7 +116,7 @@ def rect1d0(w, x):
     zero = torch.zeros(tuple(), dtype=w.dtype, device=w.device)
     ker = torch.max(torch.min(x+0.5, w/2) - torch.max(x-0.5, -w/2), zero)
     ker = ker/w
-    return(ker, x)
+    return ker, x
 
 
 def rect1d1(w, x):
@@ -110,7 +132,7 @@ def rect1d1(w, x):
     ker = integrate_poly(neg_low, neg_upp, one,  one) \
         + integrate_poly(pos_low, pos_upp, one, -one)
     ker = ker/w
-    return(ker, x)
+    return ker, x
 
 
 def triangle1d0(w, x):
@@ -126,7 +148,7 @@ def triangle1d0(w, x):
     ker = integrate_poly(neg_low, neg_upp, one,  1/w) \
         + integrate_poly(pos_low, pos_upp, one, -1/w)
     ker = ker/w
-    return(ker, x)
+    return ker, x
 
 
 def triangle1d1(w, x):
@@ -148,7 +170,7 @@ def triangle1d1(w, x):
         + integrate_poly(pos_neg_low, pos_neg_upp, 1-x/w,  1-x/w+1/w,  1/w) \
         + integrate_poly(pos_pos_low, pos_pos_upp, 1-x/w, -1+x/w+1/w, -1/w)
     ker = ker/w
-    return(ker, x)
+    return ker, x
 
 
 smooth_switcher = {
@@ -192,7 +214,7 @@ def smooth(type, fwhm=1, basis=0, x=None, sep=True):
             Default: True
 
     Returns:
-        If `sep==False` or all input parameters are scalar: a `torch.Tensor`
+        If `sep is False` or all input parameters are scalar: a `torch.Tensor`
         Else: a tuple of `torch.Tensor`
 
 
@@ -226,6 +248,7 @@ def smooth(type, fwhm=1, basis=0, x=None, sep=True):
         shape = [1, ] * nker
         shape[d] = ker1.numel()
         ker1 = ker1.reshape(shape)
+        ker1 = ker1.unsqueeze(0).unsqueeze(0)  # Cout = 1, Cin = 1
         ker += (ker1, )
 
     # Make N-D kernel
@@ -237,4 +260,219 @@ def smooth(type, fwhm=1, basis=0, x=None, sep=True):
     elif not return_tuple:
         ker = ker[0]
 
-    return(ker)
+    return ker
+
+
+def energy(dim, absolute=0, membrane=0, bending=0, lame=(0, 0), vs=1,
+           displacement=False, dtype=None, device=None):
+    """Generate a convolution kernel for a mixture of differential energies.
+
+    This function builds a convolution kernel that embeds a mixture of
+    differential energies. In practice, this energy can be computed as
+    E = <f,k*f>, where <.,.> is the Eucldean dot product.
+    Possible energies are:
+        . absolute = sum of squared absolute values
+        . membrane = sum of squared first derivatives
+        . bending  = sum of squared second derivatives (diagonal terms only)
+        . lame     = linear elastic energy
+            [0] sum of divergences  (Lame's 1st parameter, lambda)
+            [1] sum of shears       (Lame's 2nd parameter, mu)
+
+    Note: The lame parameters should be entered in the opposite order from SPM
+          SPM: (mu, lambda) / nitorch: (lambda, mu)
+
+    Args:
+        dim (int): Dimension of the problem (1, 2, or 3)
+        absolute (float, optional): Defaults to 0.
+        membrane (float, optional): Defaults to 0.
+        bending (float, optional): Defaults to 0.
+        lame (float, optional): Defaults to (0, 0).
+        vs (float, optional): Defaults to 1.
+        displacement (bool, optional): True if input field is a displacement
+            field. Defaults to True if `linearelastic != (0, 0)`, else False.
+        dtype (torch.dtype, optional)
+        device (torch.device, optional)
+
+    Raises:
+        ValueError: DESCRIPTION.
+
+    Returns:
+        ker (TYPE): DESCRIPTION.
+
+    """
+    # Check arguments
+    absolute = torch.as_tensor(absolute)
+    if absolute.numel() != 1:
+        raise ValueError('The absolute energy must be parameterised by '
+                         'exactly one parameter. Received {}.'
+                         .format(absolute.numel()))
+    absolute = absolute.flatten()[0]
+    membrane = torch.as_tensor(membrane)
+    if membrane.numel() != 1:
+        raise ValueError('The membrane energy must be parameterised by '
+                         'exactly one parameter. Received {}.'
+                         .format(membrane.numel()))
+    membrane = membrane.flatten()[0]
+    bending = torch.as_tensor(bending)
+    if bending.numel() != 1:
+        raise ValueError('The bending energy must be parameterised by '
+                         'exactly one parameter. Received {}.'
+                         .format(bending.numel()))
+    bending = bending.flatten()[0]
+    lame = torch.as_tensor(lame)
+    if lame.numel() != 2:
+        raise ValueError('The linear-elastic energy must be parameterised by '
+                         'exactly two parameters. Received {}.'
+                         .format(lame.numel()))
+    lame = lame.flatten()
+
+    if (lame != 0).any():
+        displacement = True
+
+    # Kernel size
+    if bending != 0:
+        kdim = 5
+    elif membrane != 0 or (lame != 0).any():
+        kdim = 3
+    elif absolute != 0:
+        kdim = 1
+    else:
+        kdim = 0
+
+    # Compute 1/vs^2
+    vs = torch.as_tensor(vs).flatten()
+    vs = torch.cat((vs, vs[-1].repeat(max(0, dim-vs.numel()))))
+    vs = 1./(vs*vs)
+
+    # Accumulate energies
+    ker = torch.zeros((1, 1) + (kdim,)*dim, dtype=dtype, device=device)
+    if absolute != 0:
+        kpad = (kdim-1)/2
+        kpad = (kpad,)*(2*dim)
+        ker1 = utils.pad(energy_absolute(dim, vs, dtype, device), kpad)
+        if displacement:
+            ker2 = ker1
+            ker1 = torch.zeros((dim, dim) + (kdim,)*dim,
+                               dtype=dtype, device=device)
+            for d in range(dim):
+                ker1[d, d, ...] = ker2/vs[d]
+        ker = ker + absolute*ker1
+    if membrane != 0:
+        kpad = (kdim-3)/2
+        kpad = (kpad,)*(2*dim)
+        ker1 = utils.pad(energy_membrane(dim, vs, dtype, device), kpad)
+        if displacement:
+            ker2 = ker1
+            ker1 = torch.zeros((dim, dim) + (kdim,)*dim,
+                               dtype=dtype, device=device)
+            for d in range(dim):
+                ker1[d, d, ...] = ker2/vs[d]
+        ker = ker + membrane*ker1
+    if bending != 0:
+        ker1 = energy_bending(dim, vs, dtype, device)
+        if displacement:
+            ker2 = ker1
+            ker1 = torch.zeros((dim, dim) + (kdim,)*dim,
+                               dtype=dtype, device=device)
+            for d in range(dim):
+                ker1[d, d, ...] = ker2/vs[d]
+        ker = ker + bending*ker1
+    if lame[0] != 0:
+        kpad = (kdim-3)/2
+        kpad = (0, 0, 0, 0) + (kpad,)*(2*dim)
+        ker1 = utils.pad(energy_linearelastic(dim, vs, 1, dtype, device), kpad)
+        ker = ker + lame[0]*ker1
+    if lame[1] != 0:
+        kpad = (kdim-3)/2
+        kpad = (0, 0, 0, 0) + (kpad,)*(2*dim)
+        ker1 = utils.pad(energy_linearelastic(dim, vs, 2, dtype, device), kpad)
+        ker = ker + lame[1]*ker1
+
+    return ker
+
+
+def energy1d(*args, **kwargs):
+    """energy(1, absolute, membrane, bending, linearelastic, vs)."""
+    return(energy(1, *args, **kwargs))
+
+
+def energy2d(*args, **kwargs):
+    """energy(2, absolute, membrane, bending, linearelastic, vs)."""
+    return(energy(2, *args, **kwargs))
+
+
+def energy3d(*args, **kwargs):
+    """energy(3, absolute, membrane, bending, linearelastic, vs)."""
+    return(energy(3, *args, **kwargs))
+
+
+def energy_absolute(dim, vs, dtype=torch.float, device='cpu'):
+    ker = torch.ones(1, dtype=dtype, device=device)
+    ker = ker.reshape((1,)*dim)
+    ker = ker.unsqueeze(0).unsqueeze(0)
+    return ker
+
+
+def energy_membrane(dim, vs, dtype=torch.float, device='cpu'):
+    ker = torch.zeros((3,)*dim, dtype=dtype, device=device)
+    ker[(1,)*dim] = 2.*vs.sum()
+    for d in range(dim):
+        ker[tuple(0 if i == d else 1 for i in range(dim))] = -vs[d]
+        ker[tuple(2 if i == d else 1 for i in range(dim))] = -vs[d]
+    ker = ker.unsqueeze(0).unsqueeze(0)
+    return ker
+
+
+def energy_bending(dim, vs, dtype=torch.float, device='cpu'):
+    centre = 6.*vs.pow(2).sum()
+    for d in range(dim):
+        for dd in range(d+1, dim):
+            centre = centre + 8.*vs[d]*vs[dd]
+    vs = vs.reshape(vs.numel(), 1)
+    ker = torch.zeros((5,)*dim, dtype=dtype, device=device)
+    ker[(2,)*dim] = centre
+    for d in range(dim):
+        ker[tuple(1 if i == d else 2 for i in range(dim))] = -4.*vs[d]*vs.sum()
+        ker[tuple(3 if i == d else 2 for i in range(dim))] = -4.*vs[d]*vs.sum()
+        ker[tuple(0 if i == d else 2 for i in range(dim))] = vs[d]*vs[d]
+        ker[tuple(4 if i == d else 2 for i in range(dim))] = vs[d]*vs[d]
+        for dd in range(d+1, dim):
+            ker[tuple(1 if i in (d, dd) else 2
+                      for i in range(dim))] = 2*vs[d]*vs[dd]
+            ker[tuple(3 if i in (d, dd) else 2
+                      for i in range(dim))] = 2*vs[d]*vs[dd]
+            ker[tuple(1 if i == d else 3 if i == dd else 2
+                      for i in range(dim))] = 2*vs[d]*vs[dd]
+            ker[tuple(3 if i == d else 1 if i == dd else 2
+                      for i in range(dim))] = 2*vs[d]*vs[dd]
+    ker = ker.unsqueeze(0).unsqueeze(0)
+    return ker
+
+
+def energy_linearelastic(dim, vs, lame, dtype=torch.float, device='cpu'):
+    ker = torch.zeros((dim, dim) + (3,)*dim, dtype=dtype, device=device)
+    for d in range(dim):
+        ker[(d, d) + (1,)*dim] = 2.
+        if lame == 2:
+            ker[(d, d) + (1,)*dim] += 2.*vs.sum()/vs[d]
+            for dd in range(dim):
+                ker[(d, d) + tuple(0 if i == dd else 1 for i in range(dim))] \
+                    = -vs[dd]/vs[d] if d != dd else -2.
+                ker[(d, d) + tuple(2 if i == dd else 1 for i in range(dim))] \
+                    = -vs[dd]/vs[d] if d != dd else -2.
+        else:
+            ker[(d, d) + tuple(0 if i == d else 1 for i in range(dim))] \
+                = -1.
+            ker[(d, d) + tuple(2 if i == d else 1 for i in range(dim))] \
+                = -1.
+        for dd in range(d+1, dim):
+            ker[(d, dd) + tuple(0 if i in (d, dd) else 1
+                                for i in range(dim))] = -0.25
+            ker[(d, dd) + tuple(2 if i in (d, dd) else 1
+                                for i in range(dim))] = -0.25
+            ker[(d, dd) + tuple(0 if i == d else 2 if i == dd else 1
+                                for i in range(dim))] = 0.25
+            ker[(d, dd) + tuple(2 if i == d else 0 if i == dd else 1
+                                for i in range(dim))] = 0.25
+            ker[dd, d, ...] = ker[d, dd, ...]
+    return ker
