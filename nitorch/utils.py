d6a4236c2d36af774353076d7d4b062bce0956d2
Yael Balbastre <y.balbastre@ucl.ac.uk>
2020-04-29 17:09:00 +0100

    Fix/Feature(pad): fix bounds + add mode 'reflect'

--- nitorch/utils.py
+++ nitorch/utils.py
@@ -9,25 +9,24 @@ Created on Fri Apr 24 14:45:24 2020
 # TODO:
 #   . Directly use pytorch's pad when possible
 #   . check time/memory footprint
+#   . Implement modifiers for Dirichlet/Sliding boundaries
 
 import torch
 
-__all__ = ['pad']
+__all__ = ['pad', 'same_storage']
+
+
+def same_storage(x, y):
+    """Return true if `x` and `y` share the same underlying storage."""
+    return x.storage().data_ptr() == y.storage().data_ptr()
 
 
 def bound_circular(i, n):
-    nonneg = (i >= 0)
-    i[nonneg] = i[nonneg] % n
-    i[~nonneg] = (n + (i[~nonneg] % n) % n)
-    return i
+    return i % n
 
 
 def bound_replicate(i, n):
-    pre = (i < 0)
-    post = (i >= n)
-    i[pre] = 0
-    i[post] = n-1
-    return i
+    return i.clamp(min=0, max=n-1)
 
 
 def bound_symmetric(i, n):
@@ -40,10 +39,24 @@ def bound_symmetric(i, n):
     return i
 
 
+def bound_reflect(i, n):
+    if n == 1:
+        return torch.zeros(i.size(), dtype=i.dtype, device=i.device)
+    else:
+        n2 = (n-1)*2
+        pre = (i < 0)
+        i[pre] = -i[pre]
+        i = i % n2
+        post = (i >= n)
+        i[post] = n2 - i[post]
+        return i
+
+
 bounds = {
     'circular': bound_circular,
     'replicate': bound_replicate,
     'symmetric': bound_symmetric,
+    'reflect': bound_reflect,
     }
 
 
@@ -51,6 +64,7 @@ modifiers = {
     'circular': lambda x, i, n: x,
     'replicate': lambda x, i, n: x,
     'symmetric': lambda x, i, n: x,
+    'reflect': lambda x, i, n: x,
     }
 
 
@@ -63,24 +77,39 @@ def padvec(x, n, value=0):
     return x
 
 
-def pad(inp, padsize, mode='constant', value=0):
+def pad(inp, padsize, mode='constant', value=0, side=None, nn=True):
     """Pad a tensor.
 
-    This function is a bit more generic than torch's native pad (it works in
-    any dimension), but probably a bit slower:
+    This function is a bit more generic than torch's native pad, but probably
+    a bit slower:
         . works in any dimensions
+        . with any input type
+        . with arbitrarily large padding size
         . crops the tensor for negative padding values
         . augments the dimension of the input tensor if needed
+    When used with defaults parameters (side=None, nn=True), it behaves
+    exactly like `torch.nn.functional.pad`
 
     Note that:
         . 'circular'  corresponds to the boundary condition of an FFT
+        . 'reflect'   corresponds to the boundary condition of a DCT-I
         . 'symmetric' corresponds to the boundary condition of a DCT-II
 
     Args:
         inp (tensor): Input tensor.
-        padsize (tuple): Amount of padding in each dimension (pre, post).
-        mode (string): 'constant', 'replicate', 'symmetric', 'circular'
-        value: Value to pad with in mode 'constant'.
+        padsize (tuple): Amount of padding in each dimension.
+        mode (string,optional): 'constant', 'replicate', 'reflect',
+            'symmetric', 'circular'.
+            Defaults to 'constant'.
+        value (optional): Value to pad with in mode 'constant'.
+            Defaults to 0.
+        side: Use padsize to pad on left side ('pre'), right side ('post') or
+            both sides ('both'). If None, the padding side for the left and
+            right sides should be provided in alternate order.
+            Defaults to None.
+        nn (bool,optional): Neural network mode - assumes that the first two
+            dimensions are channels and should not be padded.
+            Defaults to True.
 
     Returns:
         Padded tensor.
@@ -92,10 +121,27 @@ def pad(inp, padsize, mode='constant', value=0):
                          .format(tuple(bounds.keys()) + ('constant',), mode))
     # Compute output dimensions
     inp = torch.as_tensor(inp)
-    idim = torch.tensor(inp.shape)
+    idim = torch.as_tensor(inp.size())
     padsize = torch.as_tensor(padsize, dtype=torch.int64).flatten()
-    padpre = padsize[slice(0, padsize.numel(), 2)]
-    padpost = padsize[slice(1, padsize.numel(), 2)]
+    if side == 'both':
+        padpre = padsize
+        padpost = padsize
+    elif side == 'pre':
+        padpre = padsize
+        padpost = torch.zeros(padpre.size(), dtype=padpre.dtype,
+                              device=padpre.device)
+    elif side == 'post':
+        padpost = padsize
+        padpre = torch.zeros(padpost.size(), dtype=padpost.dtype,
+                             device=padpost.device)
+    else:
+        padpre = padsize[::2]
+        padpost = padsize[1::2]
+    if nn:
+        padpre = torch.cat((torch.zeros(2, dtype=padpre.dtype,
+                                        device=padpre.device), padpre))
+        padpost = torch.cat((torch.zeros(2, dtype=padpost.dtype,
+                                         device=padpost.device), padpost))
     ndim = max(idim.numel(), padpre.numel(), padpost.numel())
     idim = padvec(idim, ndim, 1)
     padpre = padvec(padpre, ndim, 0)
