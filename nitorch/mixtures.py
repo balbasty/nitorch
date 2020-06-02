4597fbd0f1540f3019682802bc21c7973bb6bc18
Mikael Brudfors <brudfors@gmail.com>
2020-06-02 10:27:57 +0100

    Init GMM now supports input data with negative values

--- nitorch/mixtures.py
+++ nitorch/mixtures.py
@@ -535,12 +535,14 @@ class GMM(Mixture):
         # covariance
         self.Cov = torch.zeros((C, C, K), dtype=self.dt, device=self.dev)
         for c in range(C):
-            self.mu[c, :] = \
-                torch.reshape((torch.arange(K, dtype=self.dt, device=self.dev)
-                               * mx[c])/(K + 1), (1, K))
+            rng = torch.linspace(start=mn[c], end=mx[c], steps=K, dtype=self.dt, device=self.dev)
+            num_neg = sum(rng < 0)
+            num_pos = sum(rng > 0)
+            rng = torch.arange(-num_neg, num_pos, dtype=self.dt, device=self.dev)
+            self.mu[c, :] = torch.reshape((rng * mx[c])/(K + 1), (1, K))
             self.Cov[c, c, :] = \
                 torch.reshape(torch.ones(K, dtype=self.dt, device=self.dev)
-                              * ((mx[c] - mn[c])/K)**2, (1, 1, K))
+                              * ((mx[c] - mn[c])/(K))**2, (1, 1, K))
 
     def pdf(self, X, k=0, c=-1):
         """ Probability density function (pdf) of the standard normal
