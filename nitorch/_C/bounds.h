633e6d49d13a2741d85e18eb86278bbe3076a458
Yael Balbastre <y.balbastre@ucl.ac.uk>
2020-06-05 16:05:24 +0100

    doc

--- nitorch/_C/bounds.h
+++ nitorch/_C/bounds.h
@@ -4,8 +4,17 @@
 
 namespace ni {
 
-enum class BoundType : int64_t
-  {Replicate, DCT1, DCT2, DST1, DST2, DFT, Sliding, Zero};
+enum class BoundType : int64_t{
+  Replicate,    // Replicate last inbound value = clip coordinates
+  DCT1,         // Symetric w.r.t. center of the last inbound voxel
+  DCT2,         // Symetric w.r.t. edge of the last inbound voxel (=Neuman)
+  DST1,         // Antisymetric w.r.t. center of the last inbound voxel 
+  DST2,         // Antisymetric w.r.t. edge of the last inbound voxel (=Dirichlet)
+  DFT,          // Circular / Wrap arounf the FOV
+  Sliding,      // For deformation-fields only: mixture of DCT2 and DST2
+  Zero,         // Zero outside of the FOV
+  NoCheck       // /!\ Checks disabled: assume coordinates are inbound
+};
 
 using BoundVectorRef = c10::ArrayRef<BoundType>;
 
