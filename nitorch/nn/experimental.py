6127769ac85fb7106c1277254adcd9f91e4a82df
Yaël Balbastre <yael.balbastre@gmail.com>
2020-10-21 12:36:22 -0400

    Enhancement(IterativeVxm): inherit from Vxm

--- nitorch/nn/experimental.py
+++ nitorch/nn/experimental.py
@@ -2,9 +2,9 @@
 
 import torch
 import torch.nn as tnn
-from nitorch.nn.modules._base import Module
-from nitorch.nn.modules._cnn import UNet
-from nitorch.nn.modules._spatial import GridPull, GridPush, GridExp
+from .modules._base import Module
+from .modules._cnn import UNet
+from .modules._spatial import GridPull, GridPush, GridExp, VoxelMorph
 from .. import spatial
 from ..core.utils import broadcast_to
 from ..core.pyutils import make_list
@@ -156,7 +156,7 @@ class DiffeoMovie(Module):
         return frames
 
 
-class IterativeVoxelMorph(Module):
+class IterativeVoxelMorph(VoxelMorph):
     """VoxelMorph warps a source/moving image to a fixed/target image.
 
     This network aims to bridge the gap between UNets and iterative
@@ -167,14 +167,13 @@ class IterativeVoxelMorph(Module):
     applied after the UNet, its adjoint T' is applied before)
     """
 
-    def __init__(self, dim, max_iter=None, encoder=None, decoder=None, kernel_size=3,
-                 interpolation='linear', grid_bound='dft', image_bound='dct2'):
+    def __init__(self, dim, max_iter=None, *args, **kwargs):
         """
 
         Parameters
         ----------
         dim : int
-            Dimensionalityy of the input (1|2|3)
+            Dimensionality of the input (1|2|3)
         max_iter : int or [int, int], default=[1, 20]
             Number of iterations of the recurrent network.
             If list, max_iter is randomly sampled in the corresponding
@@ -192,21 +191,9 @@ class IterativeVoxelMorph(Module):
         image_bound : bound_type, default='dct2'
             Boundary conditions of the image.
         """
-        super().__init__()
-        self.push = GridPush(interpolation=interpolation,
-                             bound=image_bound)
-        self.unet = UNet(dim,
-                         input_channels=3,
-                         output_channels=dim,
-                         encoder=encoder,
-                         decoder=decoder,
-                         kernel_size=kernel_size,
-                         activation=tnn.LeakyReLU(0.2))
-        self.exp = GridExp(interpolation=interpolation,
-                           bound=grid_bound)
-        self.pull = GridPull(interpolation=interpolation,
-                             bound=image_bound)
-        self.dim = dim
+        super().__init__(dim, *args, **kwargs, _input_channels=dim+2)
+        self.push = GridPush(interpolation=self.pull.interpolation,
+                             bound=self.pull.bound)
         if max_iter is None:
             max_iter = [1, 20]
         if isinstance(max_iter, range):
@@ -217,152 +204,6 @@ class IterativeVoxelMorph(Module):
         if max_iter[0] == max_iter[1]:
             max_iter = max_iter[0]
         self.max_iter = max_iter
-        self.image_losses = []
-        self.velocity_losses = []
-        self.image_metrics = {}
-        self.velocity_metrics = {}
-
-    def add_image_loss(self, *loss_fn):
-        """Add one or more image loss functions.
-
-        The image loss should measure similarity between the deformed
-        source and target images.
-
-        Parameters
-        ----------
-        loss_fn : callable or [callable, float]
-            Function of two arguments that returns a scalar value.
-
-        """
-        self.image_losses += list(loss_fn)
-
-    def set_image_loss(self, *loss_fn):
-        """Set one or more image loss functions.
-
-        The image loss should measure similarity between the deformed
-        source and target images.
-
-        This function discards all previously held image losses.
-
-        Parameters
-        ----------
-        loss_fn : callable or [callable, float]
-            Function of two arguments that returns a scalar value.
-
-        """
-        self.image_losses = list(loss_fn)
-
-    def add_velocity_loss(self, *loss_fn):
-        """Add one or more velocity loss functions.
-
-        The velocity loss should penalize features of the velocity field.
-
-        Parameters
-        ----------
-        loss_fn : callable or [callable, float]
-            Function of one argument that returns a scalar value.
-
-        """
-        self.velocity_losses += list(loss_fn)
-
-    def set_velocity_loss(self, *loss_fn):
-        """Set one or more image loss functions.
-
-        The velocity loss should penalize features of the velocity field.
-
-        This function discards all previously held velocity losses.
-
-        Parameters
-        ----------
-        loss_fn : callable or [callable, float]
-            Function of one argument that returns a scalar value.
-
-        """
-        self.velocity_losses = list(loss_fn)
-
-    def compute_loss(self, deformed_source, target, velocity):
-        """Compute all losses."""
-        loss = []
-        for loss_fn in self.image_losses:
-            if isinstance(loss_fn, (list, tuple)):
-                _loss_fn, weight = loss_fn
-                loss_fn = lambda *a, **k: weight*_loss_fn(*a, **k)
-            loss.append(loss_fn(deformed_source, target))
-        for loss_fn in self.velocity_losses:
-            if isinstance(loss_fn, (list, tuple)):
-                _loss_fn, weight = loss_fn
-                loss_fn = lambda *a, **k: weight*_loss_fn(*a, **k)
-            loss.append(loss_fn(velocity))
-        return loss
-
-    def add_image_metric(self, **metric_fn):
-        """Add one or more image metric functions.
-
-        The image metric should measure similarity between the deformed
-        source and target images.
-
-        Parameters
-        ----------
-        metric_fn : callable or [callable, float]
-            Function of two arguments that returns a scalar value.
-
-        """
-        self.image_metrics.update(dict(metric_fn))
-
-    def set_image_metric(self, **metric_fn):
-        """Set one or more image metric functions.
-
-        The image metric should measure similarity between the deformed
-        source and target images.
-
-        This function discards all previously held image metrics.
-
-        Parameters
-        ----------
-        metric_fn : callable or [callable, float]
-            Function of two arguments that returns a scalar value.
-
-        """
-        self.image_metrics = dict(metric_fn)
-
-    def add_velocity_metric(self, **metric_fn):
-        """Add one or more velocity metric functions.
-
-        The velocity metric should penalize features of the velocity field.
-
-        Parameters
-        ----------
-        metric_fn : callable or [callable, float]
-            Function of one argument that returns a scalar value.
-
-        """
-        self.velocity_metrics.update(dict(metric_fn))
-
-    def set_velocity_metric(self, **metric_fn):
-        """Set one or more image metric functions.
-
-        The velocity metric should penalize features of the velocity field.
-
-        This function discards all previously held velocity metrics.
-
-        Parameters
-        ----------
-        metric_fn : callable or [callable, float]
-            Function of one argument that returns a scalar value.
-
-        """
-        self.velocity_metrics = dict(metric_fn)
-
-    def compute_metric(self, deformed_source, target, velocity):
-        """Compute all metrics."""
-        metric = {}
-        for key, metric_fn in self.image_metrics.items():
-            key = '{}/{}/{}'.format(self.__class__.__name__, 'image', key)
-            metric[key] = metric_fn(deformed_source, target)
-        for key, metric_fn in self.velocity_metrics.items():
-            key = '{}/{}/{}'.format(self.__class__.__name__, 'velocity', key)
-            metric[key] = metric_fn(velocity)
-        return metric
 
     def forward(self, source, target, *, _loss=None, _metric=None):
         """
@@ -374,7 +215,7 @@ class IterativeVoxelMorph(Module):
         target : tensor (batch, channel, *spatial)
             Target/fixed image
 
-        _loss : list, optional
+        _loss : dict, optional
             If provided, all registered losses are computed and appended.
         _metric : dict, optional
             If provided, all registered metrics are computed and appended.
@@ -423,30 +264,32 @@ class IterativeVoxelMorph(Module):
 
         # initialize
         ones = torch.ones((1, 1, *shape), dtype=dtype, device=device)
-        velocity = 0
+        velocity = torch.zeros((), dtype=dtype, device=device)
         grid = spatial.identity_grid(shape, dtype=dtype, device=device)
         grid = grid[None, ...]
 
         # chain operations
         for n_iter in range(max_iter):
             p_target = self.push(target, grid)
-            pp_source = self.push(self.pull(source, grid), grid)
-            pp_ones = self.push(self.pull(ones, grid), grid)
-            pp_ones = broadcast_to(pp_ones, [batch, 1, *shape])
-            source_and_target = torch.cat((pp_source, p_target, pp_ones), dim=1)
-            increment = self.unet(source_and_target, **lm)
-            increment = spatial.channel2grid(increment)
-            velocity = velocity + increment
+            # pp_source = self.push(self.pull(source, grid), grid)
+            # pp_ones = self.push(self.pull(ones, grid), grid)
+            # pp_ones = broadcast_to(pp_ones, [batch, 1, *shape])
+            velocity = broadcast_to(velocity, [batch, *shape, self.dim])
+            velocity = spatial.grid2channel(velocity)
+            source_and_target = torch.cat((source, p_target, velocity), dim=1)
+            velocity = velocity + self.unet(source_and_target, **lm)
+            velocity = spatial.channel2grid(velocity)
             grid = self.exp(velocity)
             deformed_source = self.pull(source, grid)
 
         # compute loss and metrics
         if _loss is not None:
-            assert isinstance(_loss, list)
-            _loss += self.compute_loss(deformed_source, target, velocity)
+            assert isinstance(_loss, dict)
+            losses = self.compute_loss(deformed_source, target, velocity)
+            self.update_dict(_loss, losses)
         if _metric is not None:
             assert isinstance(_metric, dict)
             metrics = self.compute_metric(deformed_source, target, velocity)
-            self.update_metrics(_metric, metrics)
+            self.update_dict(_metric, metrics)
 
         return deformed_source, velocity
