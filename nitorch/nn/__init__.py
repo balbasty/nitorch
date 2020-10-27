58038c891daae39617a462e5212d808f93caf3ca
Yaël Balbastre <yael.balbastre@gmail.com>
2020-10-27 19:03:21 -0400

    Enhancement(nn): auto import pool

--- nitorch/nn/__init__.py
+++ nitorch/nn/__init__.py
@@ -1,8 +1,10 @@
 """Modules (Networks or Layers)."""
 
+from .activations import *
 from .modules import *
 from .losses import *
 from .training import *
+from . import activations
 from . import modules
 from . import losses
 from . import training
