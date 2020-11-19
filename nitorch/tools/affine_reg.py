33abb4a1b0dee9000a63393c21503c19ed48dc18
Mikael Brudfors <brudfors@gmail.com>
2020-11-19 16:17:48 +0000

    Initial attempt

--- nitorch/tools/affine_reg.py
+++ nitorch/tools/affine_reg.py
@@ -758,6 +758,34 @@ def _do_optimisation(q, args, opt):
             callback = lambda x: _zero_mean(x, q_shape)
         # Do optimisation
         q = fmin_powell(_compute_cost, q, args=args, disp=False, callback=callback, direc=direc)
+    elif opt['optimiser'] == 'bo':
+        # Bayesian optimisation
+        # ----------
+        from bayes_opt import BayesianOptimization
+        # Make cost function compatible with BayesianOptimization library
+        # (maximises, therefore the minus)
+        def f(q1, q2, q3, q4, q5, q6):
+            return - _compute_cost(
+                np.array((q1, q2, q3, q4, q5, q6)), *args)
+        # Set affine parameter bounds
+        t_max = 20
+        o_max = 0.5
+        q_bounds = {'q1' : (-t_max, t_max),
+                    'q2' : (-t_max, t_max),
+                    'q3' : (-t_max, t_max),
+                    'q4' : (-o_max, o_max),
+                    'q5' : (-o_max, o_max),
+                    'q6' : (-o_max, o_max)}
+        # Initialise optimiser
+        optimizer = BayesianOptimization(
+            f=f, pbounds=q_bounds, random_state=1, verbose=2)
+        # Add current estimate (warm start)
+        optimizer.space.register(q, - _compute_cost(q, *args))
+        # Do optimisation
+        optimizer.maximize(init_points=5, n_iter=50)
+        # Get optimum
+        for i, (key, value) in enumerate(optimizer.max['params'].items()):
+            q[i] = value
     # Cast back to tensor
     q = q.reshape(q_shape)
     q = torch.from_numpy(q).type(dtype).to(opt['device'])
