cb6f00bd17e318793975b9ece76daac33d3cdbd2
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2021-02-23 13:50:04 -0500

    Feature(nn): Sigmoid activation with implicit class

--- nitorch/nn/activations/__init__.py
+++ nitorch/nn/activations/__init__.py
@@ -1,1 +1,2 @@
 from ._base import _map_activations
+from ._softmax import SoftMax
--- nitorch/nn/activations/_base.py
+++ nitorch/nn/activations/_base.py
@@ -2,32 +2,32 @@ import torch.nn as tnn
 
 _tnn_activations = [
     # linear units
-    'ReLU',                   # clip (, 0]
-    'ReLU6',                  # clip (, 0] and [6, )
-    'tnn.LeakyReLU',          # mult factor for negative slope
-    'tnn.PReLU',              # LeakyReLU with learnable factor
-    'tnn.RReLU',              # LeakyReLU with random factor
+    'ReLU',               # clip (, 0]
+    'ReLU6',              # clip (, 0] and [6, )
+    'LeakyReLU',          # mult factor for negative slope
+    'PReLU',              # LeakyReLU with learnable factor
+    'RReLU',              # LeakyReLU with random factor
     # sigmoid / softmax / soft functions
-    'tnn.Sigmoid',
-    'tnn.LogSigmoid',         # log(sigmod)
-    'tnn.Hardsigmoid',        # linear approximation of sigmoid
-    'tnn.Softmax',            # multivariate sigmoid
-    'tnn.LogSoftmax',         # log(softmax)
+    'Sigmoid',
+    'LogSigmoid',         # log(sigmoid)
+    'Hardsigmoid',        # linear approximation of sigmoid
+    'Softmax',            # multivariate sigmoid
+    'LogSoftmax',         # log(softmax)
     # smooth approximations
-    'tnn.Hardswish',          # 'smooth' RELU (quadratic)
-    'tnn.Softplus',           # 'smooth' ReLU (logsumexp)
-    'tnn.GELU',               # 'smooth' RELU (Gaussian cdf)
-    'tnn.ELU',                # 'smooth' ReLU (exp-1)
-    'tnn.SELU',               #               (scaled ELU)
-    'tnn.CELU',               #               (~= SELU)
-    'tnn.Softsign',           # 'smooth' sign function
+    'Hardswish',          # 'smooth' RELU (quadratic)
+    'Softplus',           # 'smooth' ReLU (logsumexp)
+    'GELU',               # 'smooth' RELU (Gaussian cdf)
+    'ELU',                # 'smooth' ReLU (exp-1)
+    'SELU',               #               (scaled ELU)
+    'CELU',               #               (~= SELU)
+    'Softsign',           # 'smooth' sign function
     # shrinkage
-    'tnn.Softshrink',         # soft-thresholding (subtract constant)
-    'tnn.Hardshrink',         # clip [-lam, +lam]
+    'Softshrink',         # soft-thresholding (subtract constant)
+    'Hardshrink',         # clip [-lam, +lam]
     # tanh
-    'tnn.Tanh',               # hyperbolic tangent
-    'tnn.Hardtanh',           # linear approximation of tanh
-    'tnn.Tanhshrink',         # shrink by tanh
+    'Tanh',               # hyperbolic tangent
+    'Hardtanh',           # linear approximation of tanh
+    'Tanhshrink',         # shrink by tanh
 ]
 _map_activations = {key.lower(): getattr(tnn, key) for key in _tnn_activations
                     if hasattr(tnn, key)}
