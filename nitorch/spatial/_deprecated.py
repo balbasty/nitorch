e2e4312696442f36fb176060e9a7ca96311fa639
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2021-03-19 13:51:21 -0400

    Deprecate(grid): move some functions to a deprecated file. I do not think they were used anywhere.

---
+++ nitorch/spatial/_deprecated.py
@@ -0,0 +1,255 @@
+import torch
+import torch.nn.functional as _F
+from nitorch.core import utils
+from ._grid import identity_grid, grid_pull
+
+
+def _vox2fov(shape, align_corners=True):
+    """Nifti to Torch coordinates.
+
+    Returns an affine matrix that transforms nifti volume coordinates
+    (in [0, len-1]) into pytorch volume coordinates (in [-1, 1]).
+
+    Args:
+        shape (tuple[int]): Shape of the volume grid (W, H, D).
+        align_corners (bool, optional): Torch coordinate type.
+            Defaults to True.
+
+    Returns:
+        mat (matrix): Affine conversion matrix (:math:`D+1 \times D+1`)
+
+    """
+    shape = torch.as_tensor(shape).to(torch.float)
+    dim = shape.numel()
+    if align_corners:
+        offset = -1.
+        scale = 2./(shape - 1.)
+    else:
+        offset = 1./shape-1.
+        scale = 2./shape
+    mat = torch.diag(torch.cat((scale, torch.ones(1))))
+    mat[:dim, -1] = offset
+    return mat
+
+
+def _fov2vox(shape, align_corners=True):
+    """Torch to Nifti coordinates."""
+    shape = torch.as_tensor(shape).to(torch.float)
+    dim = shape.numel()
+    if align_corners:
+        offset = (shape-1.)/2.
+        scale = (shape - 1.)/2.
+    else:
+        offset = (shape-1.)/2.
+        scale = shape/2.
+    mat = torch.diag(torch.cat((scale, torch.ones(1))))
+    mat[:dim, -1] = offset
+    return mat
+
+
+def _make_square(mat):
+    """Transform a compact affine matrix into a square affine matrix."""
+    mat = torch.as_tensor(mat)
+    shape = mat.size()
+    if mat.dim() != 2 or not shape[0] in (shape[1], shape[1] - 1):
+        raise ValueError('Input matrix should be Dx(D+1) or (D+1)x(D+1).')
+    if shape[0] < shape[1]:
+        addrow = torch.zeros(1, shape[1], dtype=mat.dtype, device=mat.device)
+        addrow[0, -1] = 1
+        mat = torch.cat((mat, addrow), dim=0)
+    return mat
+
+
+def _make_compact(mat):
+    """Transform a square affine matrix into a compact affine matrix."""
+    mat = torch.as_tensor(mat)
+    shape = mat.size()
+    if mat.dim() != 2 or not shape[0] in (shape[1], shape[1] - 1):
+        raise ValueError('Input matrix should be Dx(D+1) or (D+1)x(D+1).')
+    if shape[0] == shape[1]:
+        mat = mat[:-1, :]
+    return mat
+
+
+def compose(*args, interpolation='linear', bound='dft'):
+    """Compose multiple spatial deformations (affine matrices or flow fields).
+    """
+    # TODO:
+    # . add shape/dim argument to generate (if needed) an identity field
+    #   at the end of the chain.
+    # . possibility to provide fields that have an orientation matrix?
+    #   (or keep it the responsibility of the user?)
+    # . For higher order (> 1) interpolation: convert to spline coefficients.
+
+    def ismatrix(x):
+        """Check that a tensor is a matrix (ndim == 2)."""
+        x = torch.as_tensor(x)
+        shape = torch.as_tensor(x.shape)
+        return shape.numel() == 2
+
+    # Pre-pass: check dimensionality
+    dim = None
+    last_affine = False
+    at_least_one_field = False
+    for arg in args:
+        if ismatrix(arg):
+            last_affine = True
+            dim1 = arg.shape[1]
+        else:
+            last_affine = False
+            at_least_one_field = True
+            dim1 = arg.dim() - 2
+        if dim is not None and dim != dim1:
+            raise ValueError("All deformations should have the same "
+                             "dimensionality (2D/3D).")
+        elif dim is None:
+            dim = dim1
+    if at_least_one_field and last_affine:
+        raise ValueError("The last deformation cannot be an affine matrix. "
+                         "Use affine_field to transform it first.")
+
+    # First pass: compose all sequential affine matrices
+    args1 = []
+    last_affine = None
+    for arg in args:
+        if ismatrix(arg):
+            if last_affine is None:
+                last_affine = _make_square(arg)
+            else:
+                last_affine = last_affine.matmul(_make_square(arg))
+        else:
+            if last_affine is not None:
+                args1.append(last_affine)
+                last_affine = None
+            args1.append(arg)
+
+    if not at_least_one_field:
+        return last_affine
+
+    # Second pass: perform all possible "field x matrix" compositions
+    args2 = []
+    last_affine = None
+    for arg in args1:
+        if ismatrix(arg):
+            last_affine = arg
+        else:
+            if last_affine is not None:
+                new_field = arg.matmul(
+                    last_affine[:dim, :dim].transpose(0, 1)) \
+                  + last_affine[:dim, dim].reshape((1,)*(dim+1) + (dim,))
+                args2.append(new_field)
+            else:
+                args2.append(arg)
+    if last_affine is not None:
+        args2.append(last_affine)
+
+    # Third pass: compose all flow fields
+    field = args2[-1]
+    for arg in args2[-2::-1]:  # args2[-2:0:-1]
+        arg = arg - identity_grid(arg.shape[1:-1], arg.dtype, arg.device)
+        arg = utils.last2channel(arg)
+        field = field + utils.channel2last(grid_pull(arg, field, interpolation, bound))
+
+    # /!\ (TODO) The very first field (the first one being interpolated)
+    # potentially contains a multiplication with an affine matrix (i.e.,
+    # it might not be expressed in voxels). This affine transformation should
+    # be removed prior to subtracting the identity, and added back at the end.
+    # However, I don't know how to 'guess' this matrix.
+    #
+    # After further though, I think we can find the matrix that minimizes in
+    # the least-square sense (F*M-I), where F is NbVox*D and contains the
+    # deformation field, I is NbVox*D and contains the identity field
+    # (expressed in voxels) and M is the inverse of the unknown matrix.
+    # This problem has a closed form solution: (F'*F)\(F'*I).
+    # For better stability, We could encode M in gl(D), the Lie
+    # algebra of invertible matrices, and use gauss-newton to optimise
+    # the problem.
+    #
+    # Below is a tentative implementatin of the linear version
+    # > Needs F'F to be invertible and well-conditioned
+
+    # # For the last field, we factor out a possible affine transformation
+    # arg = args2[0]
+    # shape = arg.shape
+    # N = shape[0]                                     # Batch size
+    # D = shape[-1]                                    # Dimension
+    # V = torch.as_tensor(shape[1:-1]).prod()          # Nb of voxels
+    # Id = identity(arg.shape[-2:0:-1], arg.dtype, arg.device).reshape(V, D)
+    # arg = arg.reshape(N, V, D)                       # Field as a matrix
+    # one = torch.ones((N, V, 1), dtype=arg.dtype, device=arg.device)
+    # arg = cat((arg, one), 2)
+    # Id  = cat((Id, one))
+    # AA = arg.transpose(1, 2).bmm(arg)                # LHS of linear system
+    # AI = arg.transpose(1, 2).bmm(arg)                # RHS of linear system
+    # M, _ = torch.solve(AI, AA)                       # Solution
+    # arg = arg.bmm(M) - Id                            # Closest displacement
+    # arg = arg[..., :-1].reshape(shape)
+    # arg = utils.last2channel(arg)
+    # field = grid_pull(arg, field, interpolation, bound)     # Interpolate
+    # field = field + channel2grid(grid_pull(arg, field, interpolation, bound))
+    # shape = field.shape
+    # V = torch.as_tensor(shape[1:-1]).prod()
+    # field = field.reshape(N, V, D)
+    # one = torch.ones((N, V, 1), dtype=field.dtype, device=field.device)
+    # field, _ = torch.solve(field.transpose(1, 2), M.transpose(1, 2))
+    # field = field.transpose(1, 2)[..., :-1].reshape(shape)
+
+    return field
+
+
+def jacobian(warp, bound='circular'):
+    """Compute the jacobian of a 'vox' warp.
+
+    This function estimates the field of Jacobian matrices of a deformation
+    field using central finite differences: (next-previous)/2.
+
+    Note that for Neumann boundary conditions, symmetric padding is usuallly
+    used (symmetry w.r.t. voxel edge), when computing Jacobian fields,
+    reflection padding is more adapted (symmetry w.r.t. voxel centre), so that
+    derivatives are zero at the edges of the FOV.
+
+    Note that voxel sizes are not considered here. The flow field should be
+    expressed in voxels and so will the Jacobian.
+
+    Args:
+        warp (torch.Tensor): flow field (N, W, H, D, 3).
+        bound (str, optional): Boundary conditions. Defaults to 'circular'.
+
+    Returns:
+        jac (torch.Tensor): Field of Jacobian matrices (N, W, H, D, 3, 3).
+            jac[:,:,:,:,i,j] contains the derivative of the i-th component of
+            the deformation field with respect to the j-th axis.
+
+    """
+    warp = torch.as_tensor(warp)
+    shape = warp.size()
+    dim = shape[-1]
+    ker = kernels.imgrad(dim, device=warp.device, dtype=warp.dtype)
+    ker = kernels.make_separable(ker, dim)
+    warp = utils.last2channel(warp)
+    if bound in ('circular', 'fft'):
+        warp = utils.pad(warp, (1,)*dim, mode='circular', side='both')
+        pad = 0
+    elif bound in ('reflect1', 'dct1'):
+        warp = utils.pad(warp, (1,)*dim, mode='reflect1', side='both')
+        pad = 0
+    elif bound in ('reflect2', 'dct2'):
+        warp = utils.pad(warp, (1,)*dim, mode='reflect2', side='both')
+        pad = 0
+    elif bound in ('constant', 'zero', 'zeros'):
+        pad = 1
+    else:
+        raise ValueError('Unknown bound {}.'.format(bound))
+    if dim == 1:
+        conv = _F.conv1d
+    elif dim == 2:
+        conv = _F.conv2d
+    elif dim == 3:
+        conv = _F.conv3d
+    else:
+        raise ValueError('Warps must be of dimension 1, 2 or 3. Got {}.'
+                         .format(dim))
+    jac = conv(warp, ker, padding=pad, groups=dim)
+    jac = jac.reshape((shape[0], dim, dim) + shape[1:])
+    jac = jac.permute((0,) + tuple(range(3, 3+dim)) + (1, 2))
+    return jac
