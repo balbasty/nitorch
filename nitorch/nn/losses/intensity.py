c122b47ea20d1b84b3da58b82d7fdf2c34cb8a72
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2021-03-26 16:01:41 -0400

    Perf(MutualInfoLoss): faster mutual info using spline-based joint histogram

--- nitorch/nn/losses/intensity.py
+++ nitorch/nn/losses/intensity.py
@@ -5,12 +5,137 @@ Losses for intensity (continuous) data.
 import torch
 import math
 from .base import Loss
+from nitorch.core import py, utils
 from nitorch.core.py import make_list
 from nitorch.core.utils import unsqueeze, channel2last, last2channel
 from nitorch.core.constants import eps, nan, inf, pi
 from nitorch.core.math import nanmin, nanmax, nansum
 from nitorch.core.linalg import matvec
 from nitorch.vb.mixtures import GMM
+
+
+def joint_hist_gaussian(x, y, bins=64, min=None, max=None, fwhm=1, mask=None):
+    """Compute joint histogram with Gaussian window
+
+    Parameters
+    ----------
+    x : (batch, channel, voxels) tensor
+    y : (batch, channel, voxels) tensor
+    bins : int or (int, int), default=64
+    min : float or (float, float), optional
+    max : float or (float, float), optional
+    fwhm : float or (float, float), default=1
+    mask : (batch, channel, voxels) tensor, optional
+
+    Returns
+    -------
+    h : (batch, channel, bins, bins)
+
+    """
+    backend = utils.backend(x)
+    x_min, y_min = py.make_list(min, 2)
+    x_max, y_max = py.make_list(max, 2)
+    x_nbins, y_nbins = py.make_list(bins, 2)
+    x_fwhm, y_fwhm = py.make_list(fwhm, 2)
+
+    def get_bins(x, min, max, nbins):
+        """Compute the histogram bins."""
+        # TODO: It's suboptimal to have bin centers fall at the
+        #   min and max. Better to shift them slightly inside.
+        if mask is not None:
+            # we set masked values to nan so that we can exclude them when
+            # computing min/max
+            val_nan = torch.as_tensor(nan, **backend)
+            x = torch.where(mask, val_nan, x)
+            min_fn = nanmin
+            max_fn = nanmax
+        else:
+            min_fn = torch.min
+            max_fn = torch.max
+        min = min_fn(x, dim=-1).values if min is None else min
+        min = torch.as_tensor(min, **backend)
+        min = unsqueeze(min, dim=2, ndim=4 - min.dim())
+        # -> shape = [B, C, 1, 1]
+        max = max_fn(x, dim=-1).values if max is None else max
+        max = torch.as_tensor(max, **backend)
+        max = unsqueeze(max, dim=2, ndim=4 - max.dim())
+        # -> shape = [B, C, 1, 1]
+        bins = torch.linspace(0, 1, nbins, **backend)
+        bins = unsqueeze(bins, dim=0, ndim=3)  # -> [1, 1, 1, nb_bins]
+        bins = min + bins * (max - min)  # -> [B, C, 1, nb_bins]
+        binwidth = (max - min) / (nbins - 1)  # -> [B, C, 1, 1]
+        return bins, binwidth
+
+    # prepare bins
+    x_bins, x_binwidth = get_bins(x.detach(), x_min, x_max, x_nbins)
+    y_bins, y_binwidth = get_bins(y.detach(), y_min, y_max, y_nbins)
+
+    # we transform our nans into inf so that they get zero-weight
+    # in the histogram
+    if mask is not None:
+        val_inf = torch.as_tensor(inf, **backend)
+        x = torch.where(mask, val_inf, x)
+        y = torch.where(mask, val_inf, y)
+
+    # compute distances and collapse
+    x = x[..., None]  # -> [B, C, N, 1]
+    y = y[..., None]  # -> [B, C, N, 1]
+    x_var = ((x_fwhm * x_binwidth) ** 2) / (8 * math.log(2))
+    x_var = x_var.clamp(min=eps(x.dtype))
+    x = -(x - x_bins).square() / (2 * x_var)
+    x = x.exp()
+    y_var = ((y_fwhm * y_binwidth) ** 2) / (8 * math.log(2))
+    y_var = y_var.clamp(min=eps(y.dtype))
+    y = -(y - y_bins).square() / (2 * y_var)
+    y = y.exp()
+    # -> [B, C, N, nb_bins]
+
+    x = x.transpose(-1, -2)
+    h = torch.matmul(x, y)   # -> [B, C, nb_bins, nb_bins]
+    return h
+
+
+def joint_hist_spline(x, y, bins=64, min=None, max=None, order=3, mask=None):
+    """Compute joint histogram with Gaussian window
+
+    Parameters
+    ----------
+    x : (batch, channel, voxels) tensor
+    y : (batch, channel, voxels) tensor
+    bins : int or (int, int), default=64
+    min : float or (float, float), optional
+    max : float or (float, float), optional
+    order : 0..7, default=3
+    mask : (batch, channel, voxels) tensor, optional
+
+    Returns
+    -------
+    h : (batch, channel, bins, bins)
+
+    """
+
+    # compute limits
+    x_min, y_min = py.make_list(min, 2)
+    x_max, y_max = py.make_list(max, 2)
+    if x_min is None:
+        x_min = x.min(dim=-1, keepdim=True).values
+    if y_min is None:
+        y_min = y.min(dim=-1, keepdim=True).values
+    if x_max is None:
+        x_max = x.max(dim=-1, keepdim=True).values
+    if y_max is None:
+        y_max = y.max(dim=-1, keepdim=True).values
+    min_val = [x_min, y_min]
+    max_val = [x_max, y_max]
+
+    if mask is not None:
+        # set masked values outside of the [min, max] range
+        val_inf = torch.as_tensor(inf, **utils.backend(x))
+        x = torch.where(mask, val_inf, x)
+        y = torch.where(mask, val_inf, y)
+
+    return utils.histc2(torch.stack([x, y], -1), bins, min_val, max_val,
+                        dim=-2, order=order, bound='zero')
 
 
 class MutualInfoLoss(Loss):
@@ -25,7 +150,7 @@ class MutualInfoLoss(Loss):
        and less values need to be modelled.
     """
 
-    def __init__(self, min_val=None, max_val=None, nb_bins=32, fwhm=1,
+    def __init__(self, min_val=None, max_val=None, nb_bins=32, order=3, fwhm=1,
                  normalize='arithmetic', patch_size=None, patch_stride=None,
                  mask=None, *args, **kwargs):
         """
@@ -36,8 +161,11 @@ class MutualInfoLoss(Loss):
             Minimum value in the joint histogram (per image if list).
         max_val : float or [float, float], default=max of input
             Maximum value in the joint histogram (per image if list).
-        nb_bins : int or [int, int], default=32
+        nb_bins : int or [int, int], default=128
             Number of bins in the histogram (per image if list).
+        order : 0..7 or 'inf', default=3
+            Order of the B-splines encoding the histogram.
+            If 'inf', use Gaussian window with width `fwhm`.
         fwhm : float or [float, float], default=1
             Full-width half-max of the Gaussian window, in bins.
         normalize : bool or str or callable, default='arithmetic'
@@ -61,6 +189,7 @@ class MutualInfoLoss(Loss):
         self.min_val = min_val
         self.max_val = max_val
         self.nb_bins = nb_bins
+        self.order = order
         self.fwhm = fwhm
         self.normalize = normalize
         self.patch_size = patch_size
@@ -88,8 +217,6 @@ class MutualInfoLoss(Loss):
         # check inputs
         x = torch.as_tensor(x)
         y = torch.as_tensor(y)
-        dtype = x.dtype
-        device = x.device
         nb_dim = x.dim() - 2
         if x.shape[1] != 1 or y.shape[1] != 1:
             raise ValueError('Mutual info is only implemented for '
@@ -97,10 +224,11 @@ class MutualInfoLoss(Loss):
         shape = x.shape[2:]
 
         # get parameters
-        x_min, y_min = make_list(overload.get('min_val', self.min_val), 2)
-        x_max, y_max = make_list(overload.get('max_val', self.max_val), 2)
-        x_nbins, y_nbins = make_list(overload.get('nb_bins', self.nb_bins), 2)
-        x_fwhm, y_fwhm = make_list(overload.get('fwhm', self.fwhm), 2)
+        min_val = overload.get('min_val', self.min_val)
+        max_val = overload.get('max_val', self.max_val)
+        nb_bins = overload.get('nb_bins', self.nb_bins)
+        fwhm = overload.get('fwhm', self.fwhm)
+        order = overload.get('order', self.order)
         normalize = overload.get('normalize', self.normalize)
         patch_size = overload.get('patch_size', self.patch_size)
         patch_stride = overload.get('patch_stride', self.patch_stride)
@@ -111,18 +239,9 @@ class MutualInfoLoss(Loss):
             # extract patches about each voxel
             patch_size = make_list(patch_size, nb_dim)
             patch_size = [pch or dim for pch, dim in zip(patch_size, shape)]
-            patch_stride = make_list(patch_stride, nb_dim)
-            patch_stride = [sz if st is None else st
-                            for sz, st in zip(patch_size, patch_stride)]
-            x = x[:, 0, ...]
-            y = y[:, 0, ...]
-            for d, (sz, st) in enumerate(zip(patch_size, patch_stride)):
-                x = x.unfold(dimension=d + 1, size=sz, step=st)
-                y = y.unfold(dimension=d + 1, size=sz, step=st)
-            x = x.reshape((x.shape[0], -1, *patch_size))
-            y = y.reshape((y.shape[0], -1, *patch_size))
-            # now, the spatial dimension of x and y is `patch_size` and
-            # their channel dimension is the number of patches
+            x = utils.unfold(x[:, 0], patch_size, patch_stride, collapse=True)
+            y = utils.unfold(y[:, 0], patch_size, patch_stride, collapse=True)
+
         # collapse spatial dimensions -> we don't need them anymore
         x = x.reshape((*x.shape[:2], -1))
         y = y.reshape((*y.shape[:2], -1))
@@ -139,57 +258,10 @@ class MutualInfoLoss(Loss):
         elif mask_y is not None:
             mask = (mask & (y <= mask_y)) if mask is not None else (y <= mask_y)
 
-        def get_bins(x, min, max, nbins):
-            """Compute the histogram bins."""
-            # TODO: It's suboptimal to have bin centers fall at the
-            #   min and max. Better to shift them slightly inside.
-            if mask is not None:
-                # we set masked values to nan so that we can exclude them when
-                # computing min/max
-                val_nan = torch.as_tensor(nan, dtype=x.dtype, device=x.device)
-                x = torch.where(mask, val_nan, x)
-                min_fn = nanmin
-                max_fn = nanmax
-            else:
-                min_fn = torch.min
-                max_fn = torch.max
-            min = min_fn(x, dim=-1).values if min is None else min
-            min = torch.as_tensor(min, dtype=dtype, device=device)
-            min = unsqueeze(min, dim=2, ndim=4 - min.dim())
-            # -> shape = [B, C, 1, 1]
-            max = max_fn(x, dim=-1).values if max is None else max
-            max = torch.as_tensor(max, dtype=dtype, device=device)
-            max = unsqueeze(max, dim=2, ndim=4 - max.dim())
-            # -> shape = [B, C, 1, 1]
-            bins = torch.linspace(0, 1, nbins, dtype=dtype, device=device)
-            bins = unsqueeze(bins, dim=0, ndim=3)   # -> [1, 1, 1, nb_bins]
-            bins = min + bins * (max - min)         # -> [B, C, 1, nb_bins]
-            binwidth = (max-min)/(nbins-1)          # -> [B, C, 1, 1]
-            return bins, binwidth
-
-        # prepare bins
-        x_bins, x_binwidth = get_bins(x.detach(), x_min, x_max, x_nbins)
-        y_bins, y_binwidth = get_bins(y.detach(), y_min, y_max, y_nbins)
-
-        # we transform our nans into inf so that they get zero-weight
-        # in the histogram
-        if mask is not None:
-            val_inf = torch.as_tensor(inf, dtype=x.dtype, device=x.device)
-            x = torch.where(mask, val_inf, x)
-            y = torch.where(mask, val_inf, y)
-
-        # compute distances and collapse
-        x = x[..., None]                            # -> [B, C, N, 1]
-        y = y[..., None]                            # -> [B, C, N, 1]
-        x_var = ((x_fwhm * x_binwidth) ** 2) / (8 * math.log(2))
-        x_var = x_var.clamp(min=eps(x.dtype))
-        x = -(x - x_bins).square() / (2 * x_var)
-        x = x.exp()
-        y_var = ((y_fwhm * y_binwidth) ** 2) / (8 * math.log(2))
-        y_var = y_var.clamp(min=eps(y.dtype))
-        y = -(y - y_bins).square() / (2 * y_var)
-        y = y.exp()
-        # -> [B, C, N, nb_bins]
+        if order == 'inf':
+            p_xy = joint_hist_gaussian(x, y, nb_bins, min_val, max_val, fwhm, mask)
+        else:
+            p_xy = joint_hist_spline(x, y, nb_bins, min_val, max_val, order, mask)
 
         def pnorm(x, dims=-1):
             """Normalize a tensor so that it's sum across `dims` is one."""
@@ -199,10 +271,8 @@ class MutualInfoLoss(Loss):
             return x
 
         # compute probabilities
-        p_x = pnorm(x.sum(dim=2))                 # -> [B, C, nb_bins]
-        p_y = pnorm(y.sum(dim=2))                 # -> [B, C, nb_bins]
-        x = x.transpose(-1, -2)                   # -> [B, C, nb_bins, N]
-        p_xy = torch.matmul(x, y)                 # -> [B, C, nb_bins, nb_bins]
+        p_x = pnorm(p_xy.sum(dim=-2))  # -> [B, C, nb_bins]
+        p_y = pnorm(p_xy.sum(dim=-1))  # -> [B, C, nb_bins]
         p_xy = pnorm(p_xy, [-1, -2])
 
         # compute entropies
