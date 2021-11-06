9f200b4d4cb2a8304c51978af8cc3b8c5d5de19c
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-11-06 16:34:19 +0000

    FEAT(nn.linear): LinearBlock and MLP

--- nitorch/nn/modules/linear.py
+++ nitorch/nn/modules/linear.py
@@ -1,6 +1,10 @@
+import inspect
 import torch.nn as tnn
 from nitorch.core import py, utils
-from ..base import Module
+from ..base import Module, Sequential, ModuleList
+from ..activations import make_activation_from_name
+from .norm import make_norm_from_name
+from .conv import _get_dropout_class
 
 
 def _defer_property(prop, module, setter=False):
@@ -49,3 +53,72 @@ class Linear(Module):
     in_channels = _defer_property('in_features', 'linear')
     out_channels = _defer_property('out_features', 'linear')
     bias = _defer_property('bias', 'linear')
+
+
+class LinearBlock(Sequential):
+    """
+    Block of Linear-Norm-Activation.
+    TODO: Add functionality to actually use dropout, and to use custom order like in Conv
+    """
+    def __init__(self, in_channels, out_channels, norm=None, activation=None,
+                 dim=3, linear_dim=1, bias=True, dropout=None):
+        super().__init__()
+        self.linear = Linear(in_channels, out_channels, bias, linear_dim)
+        
+        if isinstance(activation, str):
+            activation = make_activation_from_name(activation)
+        self.activation = activation
+        
+        if isinstance(norm, bool) and norm:
+            norm = 'batch'
+        if isinstance(norm, str):
+            norm = make_norm_from_name(norm, dim, in_channels)
+        self.norm = norm
+        
+        dropout = (dropout() if inspect.isclass(dropout)
+                   else dropout if callable(dropout)
+                   else _get_dropout_class(dim)(p=float(dropout)) if dropout
+                   else None)
+        self.dropout = dropout
+
+    def forward(self, x):
+        # if self.dropout is not None:
+        #     ...
+        x = self.linear(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        return x
+
+
+class MLP(Module):
+    """
+    Simple Multi-Layer Perceptron. Useful for projection head in contrastive learning.
+    """
+    def __init__(self, in_channels, out_channels, hidden_channels=[2048], 
+                 dim=3, linear_dim=1, bias=True, norm=None, 
+                 activation=None, dropout=None, final_activation=None, final_norm=None):
+        super().__init__()
+        if isinstance(hidden_channels, int):
+            hidden_channels = [hidden_channels]
+        self.first_layer = LinearBlock(in_channels, hidden_channels[0], norm=norm,
+                                        activation=activation, dim=dim, linear_dim=linear_dim,
+                                        bias=bias, dropout=dropout)
+        if len(hidden_channels) > 1:
+            self.hidden_layers = ModuleList([
+                LinearBlock(hidden_channels[i], hidden_channels[i+1], norm=norm,
+                            activation=activation, dim=dim, linear_dim=linear_dim,
+                            bias=bias, dropout=dropout) for i in len(hidden_channels)
+            ])
+        else:
+            self.hidden_layers = None
+        self.final_layer = LinearBlock(hidden_channels[-1], out_channels, norm=final_norm,
+                                        activation=final_activation, dim=dim, linear_dim=linear_dim,
+                                        bias=bias, dropout=dropout)
+
+    def forward(self, x):
+        x = self.first_layer(x)
+        if self.hidden_layers:
+            for layer in self.hidden_layers:
+                x = layer(x)
+        x = self.final_layer(x)
+        return x
