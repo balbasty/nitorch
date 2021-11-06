8a34b451058c69fc9adbbecc113a37606129292d
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-11-06 13:32:50 +0000

    ENH(nn.losses._wip_adversarial): Loss functions

--- nitorch/nn/losses/_wip_adversarial.py
+++ nitorch/nn/losses/_wip_adversarial.py
@@ -1,8 +1,10 @@
 # losses for adversarial training e.g. GANs
 import torch
+from .base import Loss
+from torch.nn.functional import softplus
 
-def Wasserstein(discriminator,
-                real, fake):
+
+def Wasserstein(discriminator, real, fake):
     """
     Wasserstein gradient penalty for W-GAN, based on @eriklindernoren on GitHub
     """
@@ -41,19 +43,91 @@ def Wasserstein(discriminator,
     return gp
 
 
-def R1(discriminator, x_in):
+def R1(discriminator, real, fake=None):
     """
     R1 penalty for discriminator regularisation.
     Based on advice from Guilherme Pombo
     """
-    x_in.requires_grad = True
-    d_out = discriminator(x_in)
+    real.requires_grad = True
+    d_out = discriminator(real)
     # zero-centered gradient penalty for real images
-    batch_size = x_in.size(0)
+    batch_size = real.size(0)
     grad_dout = torch.autograd.grad(
-        outputs=d_out.sum(), inputs=x_in,
+        outputs=d_out.sum(), inputs=real,
         create_graph=True, retain_graph=True, only_inputs=True)[0]
     grad_dout2 = grad_dout.pow(2)
-    assert (grad_dout2.size() == x_in.size())
+    assert (grad_dout2.size() == real.size())
     reg = 0.5 * grad_dout2.view(batch_size, -1).sum(1).mean(0)
     return reg
+
+
+class DiscriminatorLoss(Loss):
+    """
+    General class for discriminator loss function in GAN-style training.
+    """
+    def __init__(self,
+                 discriminator,
+                 gradient_penalty=None,
+                 softplus=False,
+                 lambda_gp=1,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = discriminator
+        if gradient_penalty == 'wasserstein' or 'Wasserstein' or 'wgan' or 'WGAN':
+            gradient_penalty = Wasserstein
+        elif self.gradient_penalty == 'r1' or 'R1':
+            gradient_penalty = R1
+        self.gradient_penalty = gradient_penalty
+        self.lambda_gp = lambda_gp
+        self.softplus = softplus
+
+    def forward(self, real, fake, model=None):
+        loss = 0
+        if not model:
+            model = self.model
+        model.eval()
+
+        if self.gradient_penalty is not None:
+            loss += self.gradient_penalty(model, real, fake) * self.lambda_gp
+
+        pred_real = model(real)
+        pred_fake = model(fake)
+
+        if self.softplus:
+            loss += torch.mean(softplus(-pred_real))
+            loss += torch.mean(softplus(pred_fake))
+        else:
+            loss -= torch.mean(pred_real)
+            loss += torch.mean(pred_fake)
+
+        return loss
+        
+
+class GeneratorLoss(Loss):
+    """
+    General class for generator loss function in GAN-style training.
+    """
+    def __init__(self,
+                 discriminator,
+                 softplus=False,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = discriminator
+        self.softplus = softplus
+
+    def forward(self, fake, model=None):
+        loss = 0
+        if not model:
+            model = self.model
+        model.eval()
+
+        pred_fake = model(fake)
+
+        if self.softplus:
+            loss += torch.mean(softplus(-pred_fake))
+        else:
+            loss -= torch.mean(pred_fake)
+
+        return loss
