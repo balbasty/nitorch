4a451a98d16cf49f6b2c347e9ebac5bbdda873ca
John Ashburner <j.ashburner@ucl.ac.uk>
2021-11-09 17:16:54 -0500

    FEAT(vb): Chi mixture

--- nitorch/core/math.py
+++ nitorch/core/math.py
@@ -4,6 +4,7 @@
 
 import torch
 import math as pymath
+from typing import List
 from .optionals import custom_fwd, custom_bwd
 from .constants import inf, ninf
 from nitorch.core import py, utils
@@ -1144,9 +1145,10 @@ def besseli(nu, z, mode=None):
 
     Returns
     -------
-    besseli(nu,z)        if mode is None
-    besseli(ni,z)/exp(z) if mode == 'norm'
-    log(besseli(nu,z))   if mode == 'log'
+    b : tensor
+        besseli(nu,z)        if mode is None
+        besseli(ni,z)/exp(z) if mode == 'norm'
+        log(besseli(nu,z))   if mode == 'log'
 
     References
     ----------
@@ -1155,6 +1157,7 @@ def besseli(nu, z, mode=None):
           Garber DP.
           NASA, Langley Research Center (1993)
     """
+    z = torch.as_tensor(z)
     if not isinstance(mode, int):
         code = (2 if mode == 'log' else 1 if mode == 'norm' else 0)
     else:
@@ -1178,9 +1181,10 @@ def besseli0(z, code: int = 0):
 
     Returns
     -------
-    besseli(0,z)        if code==0
-    besseli(0,z)/exp(z) if code==1  ('norm')
-    log(besseli(0,z))   if code==2  ('log')
+    b : tensor
+        besseli(0,z)        if code==0
+        besseli(0,z)/exp(z) if code==1  ('norm')
+        log(besseli(0,z))   if code==2  ('log')
     """
     f = torch.zeros_like(z)
     msk = z < 15.0/4.0
@@ -1223,9 +1227,10 @@ def besseli1(z, code: int = 0):
 
     Returns
     -------
-    besseli(1,z)        if code==0
-    besseli(1,z)/exp(z) if code==1  ('norm')
-    log(besseli(1,z))   if code==2  ('log')
+    b : tensor
+        besseli(1,z)        if code==0
+        besseli(1,z)/exp(z) if code==1  ('norm')
+        log(besseli(1,z))   if code==2  ('log')
     """
     f = torch.zeros_like(z)
     msk = z < 15.0/4.0
@@ -1268,9 +1273,10 @@ def besseli_small(nu: float, z, M: int = 64, code: int = 0):
 
     Returns
     -------
-    besseli(nu,z)        if code==0
-    besseli(nu,z)/exp(z) if code==1 ('norm')
-    log(besseli(nu,z))   if code==2 ('log')
+    b : tensor
+        besseli(nu,z)        if code==0
+        besseli(nu,z)/exp(z) if code==1 ('norm')
+        log(besseli(nu,z))   if code==2 ('log')
     """
     x = torch.log(0.5*z)
     f = torch.exp(x * nu - pymath.lgamma(nu + 1.0) - z)
@@ -1297,9 +1303,10 @@ def besseli_large(nu: float, z, code: int = 0):
 
     Returns
     -------
-    besseli(nu,z)        if code==0
-    besseli(nu,z)/exp(z) if code==1 ('norm')
-    log(besseli(nu,z))   if code==2 ('log')
+    b : tensor
+        besseli(nu,z)        if code==0
+        besseli(nu,z)/exp(z) if code==1 ('norm')
+        log(besseli(nu,z))   if code==2 ('log')
     """
 
     f = z/nu
@@ -1327,13 +1334,13 @@ def besseli_large(nu: float, z, code: int = 0):
     den = den*nu
     us = us + ttt*(0.0732421875 + tt*(-0.8912109375 + tt*(1.846462673611111 - tt*1.025812596450617)))/den
     den = den*nu
-    us = us + tt*tt*(0.112152099609375 + tt*(-2.3640869140625 +tt*(8.78912353515625 +
+    us = us + tt*tt*(0.112152099609375 + tt*(-2.3640869140625 + tt*(8.78912353515625 +
                tt*(-11.20700261622299 + tt*4.669584423426248))))/den
     den = den*nu
     us = us + tt*ttt*(0.2271080017089844 + tt*(-7.368794359479632 + tt*(42.53499874638846 +
                tt*(-91.81824154324002 + tt*(84.63621767460074 - tt*28.21207255820025)))))/den
     den = den*nu
-    us = us + ttt*ttt*(0.5725014209747314 + tt*(-26.49143048695155 +tt*(218.1905117442116 +
+    us = us + ttt*ttt*(0.5725014209747314 + tt*(-26.49143048695155 + tt*(218.1905117442116 +
                tt*(-699.5796273761326 + tt*(1059.990452528 + tt*(-765.2524681411817 +
                tt*212.5701300392171))))))/den
 
@@ -1360,9 +1367,10 @@ def besseli_any(nu: float, z, code: int = 0):
 
     Returns
     -------
-    besseli(nu,z)        if code==0
-    besseli(nu,z)/exp(z) if code==1 ('norm')
-    log(besseli(nu,z))   if code==2 ('log')
+    b : tensor
+        besseli(nu,z)        if code==0
+        besseli(nu,z)/exp(z) if code==1 ('norm')
+        log(besseli(nu,z))   if code==2 ('log')
     """
 
     if nu >= 15.0:
@@ -1374,6 +1382,70 @@ def besseli_any(nu: float, z, code: int = 0):
         f[msk] = besseli_small(nu, z[msk], int(pymath.ceil(thr*1.9+2.0)), code)
         f[~msk] = besseli_large(nu, z[~msk], code)
     return f
+
+
+@torch.jit.script
+def besseli_ratio(nu: float, X, N: int = 4, K: int = 10):
+    """Approximates ratio of the modified Bessel functions of the first kind:
+       besseli(nu+1,x)/besseli(nu,x)
+
+    Parameters
+    ----------
+    nu : float
+    X : tensor
+    N, K :  int
+        Terms in summation, higher number, better approximation.
+
+    Returns
+    -------
+    I : tensor
+        Ratio of the modified Bessel functions of the first kind.
+
+    References
+    ----------
+    ..[1] Amos DE. "Computation of modified Bessel functions and their ratios."
+          Mathematics of Computation. 1974;28(125):239-251.
+
+    """
+
+    # Begin by computing besseli(nu+1+N,x)/besseli(nu+N,x)
+
+    nu1 = nu + K
+    rk: List[torch.Tensor] = []
+    XX = X*X
+
+    # Lower bound (eq. 20a)
+    # for k=0:N, rk{k+1} = x./((nu1+k+0.5)+sqrt((nu1+k+1.5).^2+x.^2)); end
+    for k in range(0, N + 1):
+        tmp = XX + (nu1 + k + 1.5) ** 2
+        tmp = tmp.sqrt()
+        tmp = tmp + (nu1 + k + 0.5)
+        tmp = X / tmp
+        rk.append(tmp)
+
+    for m in range(N, 0, -1):
+        # Recursive updates (eq. 20b)
+        # rk{k} = x./(nu1+k+sqrt((nu1+k).^2+((rk{k+1})./rk{k}).
+        for k2 in range(1, m + 1):
+            tmp = rk[k2] / rk[k2-1]
+            tmp = tmp * XX
+            tmp = tmp + (nu1 + k2) ** 2
+            tmp = tmp.sqrt()
+            tmp = tmp + (nu1 + k2)
+            tmp = X / tmp
+            rk[k2-1] = tmp
+        rk.pop(-1)
+    result = rk.pop(0)
+
+    # Convert the result to besseli(nu+1,x)/besseli(nu,x) with
+    # backward recursion (eq. 2).
+    iX = X.reciprocal()
+    for k3 in range(K, 0, -1):
+        # result = 1. / (2. * (nu + k3) / X + result)
+        result = result.add(iX, alpha=2 * (nu + k3))
+        result = result.reciprocal()
+
+    return result
 
 
 # TODO:
