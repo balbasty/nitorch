aeadb45bfeea0ce68bb42b250dc9682fccfb231e
Yael Balbastre <yael.balbastre@gmail.com>
2021-12-10 18:06:02 -0500

    FEAT(mbf): reader for MBF's xml files

--- nitorch/mesh/_mbf.py
+++ nitorch/mesh/_mbf.py
@@ -1,13 +1,149 @@
 import re
 import torch
+from xml.etree import ElementTree
 from nitorch.core import py, utils, math
 from nitorch import spatial
 from ._inclusion import is_inside
 import copy
 
 
-def read_markers(fname, vx, split_groups=False):
+def read_contours_xml(fname):
+    """
+    Read contours coordinates from an XML file.
+    These files also contain information on the sampling scheme.
+
+    The input file looks like this:
+    ```
+    <?xml version="1.0" encoding="ISO-8859-1"?>
+    <mbf version="4.0" appname="Stereo Investigator Cleared Tissue Edition" appversion="2020.1.1" apprrid="SCR_016788" insrrid="SCR_004314">
+    <description><![CDATA[]]></description>
+    <filefacts>
+      <section sid="S1" name="Section 1" top="-108.9" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S2" name="Section 2" top="-158.4" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S3" name="Section 3" top="-207.9" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S4" name="Section 4" top="-257.4" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S5" name="Section 5" top="-306.9" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S6" name="Section 6" top="-356.4" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S7" name="Section 7" top="-405.9" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S8" name="Section 8" top="-455.4" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S9" name="Section 9" top="-504.9" cutthickness="49.5" mountedthickness="49.5"/>
+      <section sid="S10" name="Section 10" top="-554.4" cutthickness="49.5" mountedthickness="49.5"/>
+      <sectionmanager currentsection="Section 1" sectioninterval="1" startingsection="1"/>
+    </filefacts>
+    ...
+    ```
+
+    Returns
+    -------
+    sections : dict
+        ```
+        {
+            <Section ID>: {
+                'name': [str] <Section Name>,
+                'top': [float] <Z coordinate of top of section>,
+                'thickness': [float] <section thickness>
+            }
+        }
+        ```
+
+    contours : dict
+        ```
+        {
+            <ROI Name>: { <Section ID>: [
+                {
+                    'coord': [ (<x>, <y>, <z>), ... ],
+                    <property>: <value>,
+                }
+            ... ] },
+            # "inside" limit
+            'TopRight': { <Section ID>: [
+                {'coord': [
+                    (<x>, <y>, <z>),  # top-left
+                    (<x>, <y>, <z>),  # top-right
+                    (<x>, <y>, <z>),  # bottom-right
+                 ] }
+            ... ] },
+            # "outside" limit
+            'LeftBottom': { <Section ID>: [
+                {'coord': [
+                    (<x>, <y>, <z>),  # top-left
+                    (<x>, <y>, <z>),  # bottom-left
+                    (<x>, <y>, <z>),  # right
+                    (<x>, <y>, <z>),  # bottom-right
+                 ] }
+            ... ] },
+        }
+    """
+    ET = ElementTree
+    with open(fname) as file:
+        root = ET.parse(file)
+
+    # parse section info
+    sections = {}
+    for fact in root.iterfind('filefacts'):
+        for section in fact.iterfind('section'):
+            sid = section.get('sid')
+            name = section.get('name')
+            top = float(section.get('top'))
+            thickness = float(section.get('cutthickness'))
+            sections[sid] = dict(name=name, top=top, thickness=thickness)
+
+    # parse contour info
+    contours = {}
+    for contour in root.iterfind('contour'):
+        name = contour.get('name')
+        if name not in contours:
+            contours[name] = {}
+
+        # parse contour coordinates
+        for i, point in enumerate(contour.iterfind('point')):
+            sid = point.get('sid')
+            if sid not in contours[name]:
+                contours[name][sid] = []
+            if i == 0:
+                contours[name][sid].append({})
+                this_contour = contours[name][sid][-1]
+                this_contour['coord'] = []
+            this_contour['coord'] += [(float(point.get('x')),
+                                       float(point.get('y')),
+                                       float(point.get('z')))]
+
+        # parse sampling scheme
+        for property in contour.iterfind('property'):
+            if property.get('name') != 'Stereology':
+                continue
+            title = None
+            values = []
+            for elem in property.iter():
+                if elem.tag == 'l':
+                    if title is not None:
+                        this_contour[title] = values
+                        title = None
+                        values = []
+                    for subelem in elem.itertext():
+                        title = subelem
+                        break
+                if elem.tag == 'n':
+                    for subelem in elem.itertext():
+                        values.append(float(subelem))
+                        break
+
+    return sections, contours
+
+
+def read_markers(fname, vx=1e-3, split_groups=False):
     """Read marker coordinates file into freesurfer-compatible json format
+
+    The input file looks like this:
+    ```
+    ;Marker Coordinate File
+    X(um)    Y(um)     Z(um)  Marker Diameter
+    1 12108.17 -8056.31  -118.80     1.65
+    1 12845.47 -8010.11  -118.80     1.65
+    1 12852.07 -7965.56  -118.80     1.65
+    1 12804.22 -8008.46  -118.80     1.65
+    ...
+    ```
 
     Parameters are returned in voxel space. The image that was used to
     create the coordinates must be known to convert them to some sort of
@@ -149,9 +285,29 @@ def transform_pointset_dense(points, grid, type='grid', bound='dct2'):
     return points
 
 
-def read_asc(fname, vx=1):
+def read_contours_asc(fname, vx=1):
     """Read a MicroBrightField asc file that contains vertices of
     closed regions of interest.
+
+    The input file looks like this:
+    ```
+    ;	V3 text file written for MicroBrightField products.
+
+    (Description
+    )  ;  End of description
+    (Sections S1 "Section 1" -132 49.5 49.5
+     S2 "Section 2" -181.5 49.5 49.5
+     S3 "Section 3" -231 49.5 49.5
+     S4 "Section 4" -280.5 49.5 49.5
+     S5 "Section 5" -330 49.5 49.5
+     S6 "Section 6" -379.5 49.5 49.5
+     S7 "Section 7" -429 49.5 49.5
+     S8 "Section 8" -478.5 49.5 49.5
+     S9 "Section 9" -528 49.5 49.5
+     S10 "Section 10" -577.5 49.5 49.5
+    ) ; End of Sections
+    ...
+    ```
 
     Parameters
     ----------
