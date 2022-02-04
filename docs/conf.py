01ee584215a58b6c3a0faa7cce1c311e23ce017f
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2022-02-04 15:41:23 +0000

    Remove sphinxarg from ext

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinxarg.ext',
+    # 'sphinxarg.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
