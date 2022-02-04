20bd617f311e0a7aa0612b6d8a965b68f7926742
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2022-02-04 15:07:04 +0000

    Change to PDJ theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_pdj_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -47,7 +48,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
