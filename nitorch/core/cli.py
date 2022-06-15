6407cd701745821ce84335b10786440b2bbbc8ca
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2022-06-15 11:39:49 -0400

    Feat(cli): usefule converters

--- nitorch/core/cli.py
+++ nitorch/core/cli.py
@@ -2,6 +2,7 @@
 from copy import copy
 import collections
 import pprint
+from functools import wraps
 
 
 class ParseError(RuntimeError):
@@ -161,26 +162,129 @@ class ParsedStructure:
 
 class Actions:
     """A list of classmethods that can be used as actions"""
-    @classmethod
-    def store_value(cls, value):
+
+    @staticmethod
+    def store_value(value):
         return (lambda: value)
 
-    @classmethod
-    def store_true(cls):
+    @staticmethod
+    def store_true():
         return True
 
-    @classmethod
-    def store_false(cls):
+    @staticmethod
+    def store_false():
         return False
 
 
 class Validations:
 
-    @classmethod
-    def choice(cls, choices):
+    @staticmethod
+    def choice(choices):
+        """
+
+        Parameters
+        ----------
+        choices : sequence[any]
+            A list of valid choices
+
+        Returns
+        -------
+        validate : callable(any) -> bool
+            A validation that checks that the input is a valid choice
+
+        """
         def validate(value):
             return value in choices
         return validate
+
+
+class Conversions:
+
+    @staticmethod
+    def bool(x):
+        """Parse a bool, that can be represented by true/false or yes/no"""
+        if x.lower() in ('t', 'true', 'y', 'yes'):
+            return True
+        if x.lower() in ('t', 'false', 'n', 'no'):
+            return False
+        try:
+            return bool(int(x))
+        except ValueError:
+            return x
+
+    @staticmethod
+    def device(device):
+        """Parse an int that represents the parameter of a device"""
+        def _convert(x):
+            return (device, int(x))
+
+        return _convert
+
+    @staticmethod
+    def number_or_str(type=None):
+        """Parse a number. Return the input str if conversion fails"""
+
+        if isinstance(type, str):
+            # Let's assume the user forgot to call the function with
+            # a type. We'll use the default type.
+            return Conversions.number_or_str()(type)
+
+        if type is None:
+            # Try int, then float, then bool
+            def _number_or_str(x):
+                for type in (int, float, bool):
+                    try:
+                        return type(x)
+                    except ValueError:
+                        pass
+                return x
+        else:
+            def _number_or_str(x):
+                try:
+                    return type(x)
+                except ValueError:
+                    return x
+
+        return _number_or_str
+
+    @staticmethod
+    def number(type=None):
+        """Parse a number. Raise ValueError if conversion fails"""
+
+        if isinstance(type, str):
+            # Let's assume the user forgot to call the function with
+            # a type. We'll use the default type.
+            return Conversions.number_or_str()(type)
+
+        if type is None:
+            # Try int, then float, then bool
+            def _number_or_str(x):
+                for type in (int, float, bool):
+                    try:
+                        return type(x)
+                    except ValueError:
+                        pass
+                raise ValueError('Could not convert to float, int or bool')
+        else:
+            _number_or_str = type
+
+        return _number_or_str
+
+    @staticmethod
+    def range(x):
+        """Parse a range of values"""
+        if ':' not in x:
+            return int(x)
+        x = x.split(':')
+        if len(x) == 2:
+            x = [*x, '']
+        elif len(x) == 1:
+            x = ['', *x, '']
+        start, stop, step = x
+        start = int(start or 0)
+        step = int(step or 1)
+        stop = int(stop)
+        return range(start, stop, step)
 
 
 class Parsed(ParsedStructure):
@@ -210,72 +314,6 @@ class Parsed(ParsedStructure):
 
     def __str__(self):
         return pprint.pformat(self.todict(), indent=2)
-
-
-class CommandParser:
-
-    def __init__(self, name=None, help=None, title=None, add_help=True):
-        self.name = name
-        self.help = help
-        self.title = title
-        self.positionals = PositionalList()
-        self.options = OptionList()
-        self.groups = GroupList()
-        if add_help:
-            hopt = Option('help', ('-h', '--help'), nargs=0,
-                          help='Show this help', default=False)
-            self.options.append(hopt)
-
-    def _check_help_arg(self, args):
-        help_tags = None
-        opt_help = None
-        for opt in self.options:
-            if opt.name == 'help':
-                help_tags = opt.tags
-                opt_help = opt
-                break
-        if not help_tags:
-            return None
-        for i, arg in enumerate(args):
-            if arg in self.groups.tags:
-                return None
-            if arg in help_tags:
-                stops = list(self.options.tags) + list(self.groups.tags)
-                return opt_help.parse(args[i+1:], stops=stops)
-        return None
-
-    def parse(self, args):
-        help_opt = self._check_help_arg(args)
-        if help_opt is not None:
-            return Parsed(help=help_opt)
-
-        nargs = len(args)
-        alltags = list(self.options.tags) + list(self.groups.tags)
-        obj = self.positionals.parse(args, stops=alltags)
-        obj.merge(self.options.parse(args, stops=self.groups.tags))
-        obj.merge(self.groups.parse(args))
-        if args:
-            raise ParseError(f"Don't know what to do with '{args[0]}' "
-                             f"at position {nargs-len(args)}")
-        return obj
-
-    def add_option(self, *args, **kwargs):
-        if len(args) == 1 and not kwargs and isinstance(args[0], Option):
-            self.options.append(args[0])
-        else:
-            self.options.append(Option(*args, **kwargs))
-
-    def add_positional(self, *args, **kwargs):
-        if len(args) == 1 and not kwargs and isinstance(args[0], Positional):
-            self.positionals.append(args[0])
-        else:
-            self.positionals.append(Positional(*args, **kwargs))
-
-    def add_group(self, *args, **kwargs):
-        if len(args) == 1 and not kwargs and isinstance(args[0], Group):
-            self.groups.append(args[0])
-        else:
-            self.groups.append(Group(*args, **kwargs))
 
 
 def _n_to_minmax(n):
@@ -442,6 +480,29 @@ class Option:
 class Positional(Option):
     def __init__(self, name, nargs='*', help=None,
                  default=None, validation=None, convert=None):
+        """
+
+        Parameters
+        ----------
+        name : str
+            Name of the option in the results tree.
+        nargs : int or str, default='*'
+            Number of arguments to this option.
+                '{mx=1}?'      : up to `mx` arguments
+                '{mn=0}*'      : at least `mn` arguments
+                '{mn=0}*{mx}?' : `mn` to `mx` arguments
+                '{n}'          : exactly `n` arguments
+        help : str, optional
+            Help string
+        default : optional
+            A value to store in the results tree if the option is not
+            specified.
+        validation : callable(obj) -> bool, optional
+            A function to call on each argument to check that it belongs.
+        convert : callable(obj), optional
+            A function to call on each argument to convert it from
+            str to a target type.
+        """
         super().__init__(name, tuple(), '?', nargs, help, default,
                          validation, Actions.store_value(default), convert)
 
@@ -449,6 +510,25 @@ class Positional(Option):
 class Group:
 
     def __init__(self, name, tags=None, n=1, help=None, make_default=True):
+        """
+
+        Parameters
+        ----------
+        name : str
+            Name of the option in the results tree.
+        tags : str or sequence[str], default='@{name}'
+            Tags opening this option
+        n : int or str, default='?'
+            Number of times this options can appear.
+                '{mx=1}?'      : up to `mx` times
+                '{mn=0}*'      : at least `mn` times
+                '{mn=0}*{mx}?' : `mn` to `mx` times
+                '{n}'          : exactly `n` times
+        help : str, optional
+            Help string
+        make_default : bool, default=True
+            Generate a default group even if not set on the command line.
+        """
         super().__init__()
         self.name = name
         self.tags = tags or ('@' + name,)
@@ -528,6 +608,30 @@ class NamedGroup(Group):
 
     def __init__(self, name, choices, tags=None, n=1, help=None, default=None,
                  make_default=True):
+        """
+
+        Parameters
+        ----------
+        name : str
+            Name of the option in the results tree.
+        choices : list[str] or Positional
+            List of possible names.
+        tags : str or sequence[str], default='@{name}'
+            Tags opening this option
+        n : int or str, default='?'
+            Number of times this options can appear.
+                '{mx=1}?'      : up to `mx` times
+                '{mn=0}*'      : at least `mn` times
+                '{mn=0}*{mx}?' : `mn` to `mx` times
+                '{n}'          : exactly `n` times
+        help : str, optional
+            Help string
+        default : optional
+            A value to store in the results tree if the option is not
+            specified.
+        make_default : bool, default=True
+            Generate a default group even if not set on the command line.
+        """
         super().__init__(name, tags=tags, n=n, help=help, make_default=make_default)
         if not isinstance(choices, Positional):
             choices = Positional('name',
@@ -538,6 +642,43 @@ class NamedGroup(Group):
         self.suboptions = dict()
 
     def add_suboption(self, condition, *args, **kwargs):
+        """
+
+        Parameters
+        ----------
+        condition : str
+            Name for which the option should be added
+        name : str
+            Name of the option in the results tree.
+        tags : str or sequence[str], default='--{name}'
+            Tags opening this option
+        n : int or str, default='?'
+            Number of times this options can appear.
+                '{mx=1}?'      : up to `mx` times
+                '{mn=0}*'      : at least `mn` times
+                '{mn=0}*{mx}?' : `mn` to `mx` times
+                '{n}'          : exactly `n` times
+        nargs : int or str, default='*'
+            Number of arguments to this option.
+                '{mx=1}?'      : up to `mx` arguments
+                '{mn=0}*'      : at least `mn` arguments
+                '{mn=0}*{mx}?' : `mn` to `mx` arguments
+                '{n}'          : exactly `n` arguments
+        help : str, optional
+            Help string
+        default : optional
+            A value to store in the results tree if the option is not
+            specified.
+        validation : callable(obj) -> bool, optional
+            A function to call on each argument to check that it belongs.
+        action : callable(), optional
+            A function to call when the option is used without arguments.
+            Should return a value to store in the results tree.
+        convert : callable(obj), optional
+            A function to call on each argument to convert it from
+            str to a target type.
+
+        """
         if condition not in self.suboptions:
             self.suboptions[condition] = OptionList()
         if len(args) == 1 and not kwargs and isinstance(args[0], Option):
@@ -689,3 +830,84 @@ class PositionalList(TypedList):
 class GroupList(ListWithTags):
     subtype = Group
 
+
+class CommandParser:
+    """Main entry-point"""
+
+    def __init__(self, name=None, help=None, title=None, add_help=True):
+        """
+
+        Parameters
+        ----------
+        name : str, Name of the command
+        help : str, The global help string of the command
+        title : str, A title for the command [UNUSED]
+        add_help : bool, Whether to automatically add a `-h, --help` option.
+        """
+        self.name = name
+        self.help = help
+        self.title = title
+        self.positionals = PositionalList()
+        self.options = OptionList()
+        self.groups = GroupList()
+        if add_help:
+            hopt = Option('help', ('-h', '--help'), nargs=0,
+                          help='Show this help', default=False)
+            self.options.append(hopt)
+
+    def _check_help_arg(self, args):
+        help_tags = None
+        opt_help = None
+        for opt in self.options:
+            if opt.name == 'help':
+                help_tags = opt.tags
+                opt_help = opt
+                break
+        if not help_tags:
+            return None
+        for i, arg in enumerate(args):
+            if arg in self.groups.tags:
+                return None
+            if arg in help_tags:
+                stops = list(self.options.tags) + list(self.groups.tags)
+                return opt_help.parse(args[i+1:], stops=stops)
+        return None
+
+    def parse(self, args):
+        help_opt = self._check_help_arg(args)
+        if help_opt is not None:
+            return Parsed(help=help_opt)
+
+        nargs = len(args)
+        alltags = list(self.options.tags) + list(self.groups.tags)
+        obj = self.positionals.parse(args, stops=alltags)
+        obj.merge(self.options.parse(args, stops=self.groups.tags))
+        obj.merge(self.groups.parse(args))
+        if args:
+            raise ParseError(f"Don't know what to do with '{args[0]}' "
+                             f"at position {nargs-len(args)}")
+        return obj
+
+    def __call__(self, args):
+        return self.parse(args)
+
+    @wraps(Option.__init__, ('__annotations__', '__doc__'))
+    def add_option(self, *args, **kwargs):
+        if len(args) == 1 and not kwargs and isinstance(args[0], Option):
+            self.options.append(args[0])
+        else:
+            self.options.append(Option(*args, **kwargs))
+
+    @wraps(Positional.__init__, ('__annotations__', '__doc__'))
+    def add_positional(self, *args, **kwargs):
+        if len(args) == 1 and not kwargs and isinstance(args[0], Positional):
+            self.positionals.append(args[0])
+        else:
+            self.positionals.append(Positional(*args, **kwargs))
+
+    @wraps(Group.__init__, ('__annotations__', '__doc__'))
+    def add_group(self, *args, **kwargs):
+        if len(args) == 1 and not kwargs and isinstance(args[0], Group):
+            self.groups.append(args[0])
+        else:
+            self.groups.append(Group(*args, **kwargs))
