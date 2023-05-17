FIX(TS): grid_count (used wrong shape for image of ones)

nitorch/_C/_ts/pushpull.py
@@ -103,9 +103,10 @@ def grid_count(grid, shape: Optional[List[int]], bound: List[int],
     bound = pad_list_int(bound, dim)
     interpolation = pad_list_int(interpolation, dim)
     bound_fn = make_bound(bound)
+    grid_shape = grid.shape[-dim-1:-1]
     if shape is None:
-        shape = grid.shape[-dim-1:-1]
-    inp = torch.ones([1, 1] + list(shape), dtype=grid.dtype, device=grid.device)
+        shape = grid_shape
+    inp = torch.ones([1, 1, *grid_shape], dtype=grid.dtype, device=grid.device)
     is_iso1 = list_all([order == 1 for order in interpolation])
     if is_iso1:
         if dim == 3: